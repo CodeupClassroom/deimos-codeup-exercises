1725a7f0fa3467be89f4882a134bd441e9421042
Fernando Mendoza <fernandoza@gmail.com>
2019-11-20 11:38:27 -0600

    feat: add examples for primitive data types and Strings

---
+++ src/MyFirstClass.java
@@ -0,0 +1,29 @@
+public class MyFirstClass {
+
+//    fer
+
+    public static void main(String[] args) {
+        String name = "hello fer";
+        System.out.println(name);
+
+        byte aNumber = 100;
+        short age = 29000 + 100;
+        int secondNumber = age + aNumber;
+        long account = 2343242344564646324L;
+
+        float price = 4.5f;
+        double priceWithDiscount = price - 1.0f;
+
+        char dollarSign = '$';
+        char gender = 'M';
+
+        float res = (5 / 6);
+
+        boolean zachIsTheRoom = (aNumber == 100);
+
+        final String nameL = "fernando";
+//        nameL = "Luis";
+        System.out.print(nameL);
+    }
+
+}
