48a6365eeb86ad86c7acb3e7972090b40563be31
Fernando Mendoza <fernandoza@gmail.com>
2019-11-20 15:10:04 -0600

    feat: finish the demo for the exercise

---
+++ src/FirstExercise.java
@@ -0,0 +1,41 @@
+public class FirstExercise {
+
+    public static void main(String[] args) {
+
+//        int myFavoriteNumber = 90;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "3.14159";
+//
+//        float myNumber = (float) 3.14;
+//
+//        System.out.println("myNumber = " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        int myClass;
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+        int x = 2147483647;
+        x++;
+        System.out.println("x = " + x);
+        System.out.println(Integer.MAX_VALUE);
+
+    }
+
+}
