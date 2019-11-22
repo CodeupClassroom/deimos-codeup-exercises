7f46c8f405fc85a0f6d5b45515ac15629a992de2
sophiakurihara <sophia.sudiacal@gmail.com>
2019-11-22 11:42:43 -0600

    strings lecture walkthrough

---
+++ src/Bob.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("This is Bob!");
+        System.out.println("Let's start by greeting him");
+
+
+        String userInput;
+        boolean greeting;
+
+        do{
+            userInput = sc.nextLine();
+            greeting = (!userInput.equalsIgnoreCase("bye"));
+
+            if(userInput.endsWith("?")){
+                System.out.println("Sure");
+            }else if(userInput.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            }else if(userInput.trim().equals("")){
+                System.out.println("Fine. Be that way!");
+            }else{
+                System.out.println("Whatever");
+            }
+
+        }while(greeting);
+
+    }
+}
