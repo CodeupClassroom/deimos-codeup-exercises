bee9c17e016d06e7f62c9e015f7ad51b09f0ca3d
Fernando Mendoza <fernandoza@gmail.com>
2019-11-25 15:48:04 -0600

    feat: finish the exercise 4

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MethodsExercise {
@@ -6,24 +7,30 @@ public class MethodsExercise {
 
     public static void main(String[] args) {
 
-        boolean start = true;
+//        boolean start = true;
+//
+//        System.out.println("sum(1,2) = " + sum(1, 2));
+////        System.out.println(sum(1,2) == 3);
+////        System.out.println(sum(-1,2) == 1);
+//        System.out.println("sub(1,2) = " + sub(1, 2));
+//        System.out.println("multiply(3,2) = " + multiply(3, 2));
+//        System.out.println("division(5,2) = " + division(5, 2));
+//        System.out.println("modulus(5,2) = " + modulus(5, 2));
+//
+//        System.out.print("Enter a number between 1 and 10: ");
+//        System.out.println("userInput = " + getInteger(1, 10));
+//
+//        while(start){
+//            System.out.println("calculateFactorial() = " + calculateFactorial());
+//            System.out.println("Would you like to continue? (y/n)");
+//            start = scan.nextLine().equalsIgnoreCase("y");
+//        }
 
-        System.out.println("sum(1,2) = " + sum(1, 2));
-//        System.out.println(sum(1,2) == 3);
-//        System.out.println(sum(-1,2) == 1);
-        System.out.println("sub(1,2) = " + sub(1, 2));
-        System.out.println("multiply(3,2) = " + multiply(3, 2));
-        System.out.println("division(5,2) = " + division(5, 2));
-        System.out.println("modulus(5,2) = " + modulus(5, 2));
-
-        System.out.print("Enter a number between 1 and 10: ");
-        System.out.println("userInput = " + getInteger(1, 10));
-
-        while(start){
-            System.out.println("calculateFactorial() = " + calculateFactorial());
+        do {
+            System.out.println("Give me the number of sides");
+            rollDices(getInteger(1,100));
             System.out.println("Would you like to continue? (y/n)");
-            start = scan.nextLine().equalsIgnoreCase("y");
-        }
+        } while (scan.nextLine().equalsIgnoreCase("y"));
 
     }
 
@@ -74,6 +81,18 @@ public class MethodsExercise {
         }
 
         return output;
+    }
+
+
+    public static void rollDices(int sides){
+        // Create an instance of the Random class
+        Random random = new Random();
+        int random1 = random.nextInt(sides) + 1;
+        int random2 = random.nextInt(sides) + 1;
+
+        System.out.println("random1 = " + random1);
+        System.out.println("random2 = " + random2);
+
     }
 
 
