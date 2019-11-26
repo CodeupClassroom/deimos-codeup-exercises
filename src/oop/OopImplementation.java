7e2996788e32f31650ee670f33dfcca02137e97d
Fernando Mendoza <fernandoza@gmail.com>
2019-11-26 11:15:12 -0600

    feat: adds more examples for visibility and more THIS keyword
    - An Address was added for more examples, might be useful for the future

--- src/oop/OopImplementation.java
+++ src/oop/OopImplementation.java
@@ -3,10 +3,17 @@ package oop;
 public class OopImplementation {
 
     public static void main(String[] args) {
-        Person rick = new Person();
+        Person rick = new Person("Rick", "Sanchez", "123-45-6784");
+
+        Address ricksAddr = new Address(
+                "navarro",
+                "600",
+                "SAT",
+                "78205",
+                true
+        );
+
         System.out.println("rick.sayHello() = " + rick.sayHello());
-        rick.firstName = "Rick";
-        rick.lastName = "Sanchez";
         System.out.println(rick.sayHello());
         System.out.println(Person.breathe("air"));
 
@@ -14,13 +21,14 @@ public class OopImplementation {
 
         // prints "Hello from Rick Sanchez!"
 
-        Person ada = new Person();
-        ada.firstName = "Ada";
-        ada.lastName = "Lovelace";
+        Person ada = new Person("Ada", "Lovelace", "123-45-6783");
+        Person grace = new Person("Grace", "Hopper", "123-45-6781");
 
-        Person grace = new Person();
-        grace.firstName = "Grace";
-        grace.lastName = "Hopper";
+
+        System.out.println("grace.getSsn() = " + grace.getSsn());
+
+        grace.provideDocs("birthCertificate", 100);
+        ada.provideDocs("fakeBirthCertificate", 1000);
 
         System.out.println("Person.worldPopulation = " + Person.worldPopulation);
 
--- src/oop/Person.java
+++ src/oop/Person.java
@@ -6,14 +6,18 @@ public class Person {
     public String lastName;
     public static boolean liveOnEarth = true;
     public static long worldPopulation = 7_500_000_000L;
+    private String ssn;
 
 //    Executes every time that someone makes a new Object()
-    public Person(){
+//    public Person(){
+//        this.worldPopulation++;
+//    }
+
+    public Person(String firstNameExternal, String lastNameExternal, String ssnExt){
+        this.firstName = firstNameExternal;
+        this.lastName = lastNameExternal;
+        this.ssn = ssnExt;
         this.worldPopulation++;
-    }
-
-    public Person(String firstName, String lastName){
-
     }
 
     public String sayHello() {
@@ -22,6 +26,26 @@ public class Person {
 
     public static String breathe(String oxygen){
         return "CO2";
+    }
+
+//     Getter
+    public String getSsn(){
+        System.out.println("Auth process");
+        System.out.println("Verification process");
+        System.out.println("email confirmation process");
+        return this.ssn;
+    }
+
+    public void provideDocs(String docs, double price){
+        if(docs.equals("birthCertificate")){
+            this.makePayment(price);
+        } else {
+            System.out.println("Call security");
+        }
+    }
+
+    private void makePayment(double money){
+        System.out.println(this.firstName + " Paid: $" + money);
     }
 
 }
