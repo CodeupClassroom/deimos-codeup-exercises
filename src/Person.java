cb14255702930f446f0e1e400ca6e5d274b3752d
Fer <fernandoza@gmail.com>
2019-11-28 15:48:57 -0800

    feat: create constructor

--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,28 @@
 public class Person {
 
+    // The class has a private name property that is a string
     private String name;
+
+    //The class should have a constructor that accepts a String value and sets the person's name to the passed string.
+    public Person(String name) {
+        this.name = name;
+    }
 
     // returns the person's name
     public String getName(){
         return this.name;
     }
+
     // changes the name property to the passed value
     public void setName(String name){
         this.name = name;
     }
+
     // prints a message to the console using the person's name
     public void sayHello(){
         System.out.println("Hi, my name is: " + this.name);
     }
+
+    
+
 }
