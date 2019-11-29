e341df974944926702d73af72f28f114256a3a02
Fer <fernandoza@gmail.com>
2019-11-28 16:43:27 -0800

    feat: Create an input validation class

---
+++ src/util/InputTest.java
@@ -0,0 +1,15 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        System.out.println("What's your name");
+        System.out.println(input.getString());
+        System.out.println(input.getInt(1, 10));
+
+        System.out.println(input.getDouble(1, 10));
+
+        System.out.println(input.yesNo());
+    }
+}
